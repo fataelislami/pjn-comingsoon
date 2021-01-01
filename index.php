842ca559eae03e7b28794e9fc1f3bd223f40e85e
fataelislami <fataelislami97@gmail.com>
2021-01-01 23:13:00 +0700

    Update comingsoon

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Pejuang Negeri - Coming Soon</title>
+<style>
+.bg{
+    background-image: url("mobile-comingsoon.png");
+    height:100%;
+    width:100%;
+    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+@media only screen and (min-width: 768px) {
+    .bg{
+    background-image: url("desktop-comingsoon.jpg");
+    height:100%;
+    width:100%;
+    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+}
+
+</style>
+</head>
+<body style="margin:0;">
+    <div class="bg"></div>
+</body>
+</html>
